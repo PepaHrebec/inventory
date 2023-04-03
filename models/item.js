dd5dc5c209ac16de1df5d35e9dd7db9701994463
Josef Hrebec <josefhrebec02@gmail.com>
2023-04-03 16:15:05 +0200

    Add item model

---
+++ models/item.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+  item_name: { type: String, required: true, maxLength: 100 },
+  item_description: { type: String, required: true, maxLength: 100 },
+  item_category: { type: String, required: true },
+  item_price: { type: Number, required: true },
+  stock_number: { type: Number, required: true },
+});
+
+ItemSchema.virtual("url").get(function () {
+  return `${this._id}`;
+});
+
+module.exports = mongoose.model("Item", ItemSchema);
