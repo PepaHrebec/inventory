56b9237a5a8737eb2ccc7846abba08af38686134
Josef Hrebec <josefhrebec02@gmail.com>
2023-04-04 15:54:52 +0200

    Replace callbacks with promises

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -7,12 +7,22 @@ exports.index = (req, res) => {
 };
 
 exports.categories_list = (req, res, next) => {
-  Category.find().exec(function (err, list_categories) {
-    if (err) {
-      return next(err);
-    }
-    res.render("categories", {
-      categories: list_categories,
+  // Category.find().exec(function (err, list_categories) {
+  //   if (err) {
+  //     return next(err);
+  //   }
+  //   res.render("categories", {
+  //     categories: list_categories,
+  //   });
+  // });
+  Category.find({})
+    .then((resolve) => {
+      res.render("categories", {
+        title: "List",
+        categories: resolve,
+      });
+    })
+    .catch((err) => {
+      next(err);
     });
-  });
 };
