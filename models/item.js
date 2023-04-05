6bbc675c3e66d758c91e6715a95405be9e97a2ed
Josef Hrebec <josefhrebec02@gmail.com>
2023-04-05 12:42:38 +0200

    Add routes

--- models/item.js
+++ models/item.js
@@ -15,7 +15,7 @@ const ItemSchema = new Schema({
 });
 
 ItemSchema.virtual("url").get(function () {
-  return `${this._id}`;
+  return `/item/${this._id}`;
 });
 
 module.exports = mongoose.model("Item", ItemSchema);
