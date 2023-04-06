97e0bc84708d00f2a5a09d0b1d465438e10270ec
Josef Hrebec <josefhrebec02@gmail.com>
2023-04-06 14:45:14 +0200

    Add parts of item create get and post

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,4 +1,6 @@
 const Item = require("../models/item");
+const Category = require("../models/category");
+const { body, validationResult } = require("express-validator");
 
 exports.items_list = (req, res, next) => {
   Item.find({})
@@ -26,3 +28,17 @@ exports.item_details = (req, res, next) => {
       next(err);
     });
 };
+
+exports.item_create_get = (req, res, next) => {
+  res.render("item_form");
+};
+
+exports.item_create_post = [
+  body("item_name").trim().escape().isLength({ max: 100 }),
+  body("item_description").trim().escape().isLength({ max: 1000 }),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+    }
+  },
+];
